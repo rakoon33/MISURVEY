22dc1b695254592b42fdff36048170ad1d9b8030
rakoon33 <phamnam7892@gmail.com>
2023-09-14 15:50:25 +0700

    Installing dependencies and project setup
    Add tailwindcss, Angular Material and set up

--- survey-strike/src/app/app.module.ts
+++ survey-strike/src/app/app.module.ts
@@ -4,15 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/auth/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatGridListModule } from '@angular/material/grid-list' 
+import { MatMenuModule } from '@angular/material/menu'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar' 
+import { MatTableModule } from '@angular/material/table'; 
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule} from '@angular/material/snack-bar' 
+import { MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
+    SharedModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule, 
+    MatGridListModule, 
+    MatMenuModule, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatIconModule, 
+    MatExpansionModule, 
+    MatListModule, 
+    MatToolbarModule, 
+    MatTableModule, 
+    MatBadgeModule, 
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- survey-strike/src/app/shared/shared.module.ts
+++ survey-strike/src/app/shared/shared.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,14 @@ import { CapitalizePipe } from './pipes/capitalize.pipe';
     CapitalizePipe
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [  // Xuất khẩu các thành phần, directives và pipes
+    HeaderComponent,
+    HighlightDirective,
+    CapitalizePipe
   ]
 })
 export class SharedModule { }
