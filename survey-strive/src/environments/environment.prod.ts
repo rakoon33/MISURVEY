27b29e05386d03a5fff61d575c31da7b8730c3b6
rakoon33 <phamnam7892@gmail.com>
2023-09-20 10:59:25 +0700

    ignore dist, add enviroment

---
+++ survey-strive/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    // Thêm các cấu hình khác ở đây nếu cần
+  };
---
+++ survey-strive/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    // Thêm các cấu hình khác ở đây nếu cần
+  };
