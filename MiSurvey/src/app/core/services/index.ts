7b1dbc76c8dc08a2110029a21f2f6e9e3bcd551f
rakoon33 <phamnam7892@gmail.com>
2023-12-04 14:41:55 +0700

    MS-143 Add toastr

--- MiSurvey/src/app/core/services/index.ts
+++ MiSurvey/src/app/core/services/index.ts
@@ -1,6 +1,12 @@
 import { AuthService } from './auth.service';
-import { CompanyManagementService } from './company-management.service'
-import {DefaultLayoutService} from './default-layout.service';
-import {UserManagementService} from './user-management.service'
-import {UserService} from './user.service';
-export {UserService, AuthService, CompanyManagementService, DefaultLayoutService, UserManagementService}
+import { CompanyManagementService } from './company-management.service';
+import { DefaultLayoutService } from './default-layout.service';
+import { UserManagementService } from './user-management.service';
+import { UserService } from './user.service';
+export {
+  UserService,
+  AuthService,
+  CompanyManagementService,
+  DefaultLayoutService,
+  UserManagementService,
+};
